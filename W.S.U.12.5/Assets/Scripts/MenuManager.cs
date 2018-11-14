b67ddd1eb95b2046bac50615d07d584872cc4bea
JorgeLuisSuarique <jlsuarique@gmail.com>
2018-11-13 20:32:27 -0500

    part4

---
+++ W.S.U.12.5/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagememente;
+
+public class MenuManager : MonoBehaviour
+{
+    GameObject menuOpcion;
+    GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
