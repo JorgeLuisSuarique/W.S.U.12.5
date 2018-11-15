922635845eaccf923e03033e3e9e78c269de556f
jorge luis suarique <jlsuarique@gmail.com>
2018-11-15 14:48:38 -0500

    part7

---
+++ W.S.U.12.5/Assets/Scripts/CullingModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CullingModel : MonoBehaviour
+{
+    private PolygonCollider2D pc2d;
+    private EnemyManager controller;
+    private Animator animator;
+
+    private void OnBecameInvisible()
+    {
+        pc2d.enabled = false;
+        //controller.maxSpeed = 0;
+        //controller.speed = 0;
+        animator.enabled = false;
+    }
+
+    private void OnBecameVisible()
+    {
+        pc2d.enabled = true;
+        //controller.maxSpeed = 1;
+        //controller.speed = 3.5f;
+        animator.enabled = true;
+    }
+
+    private void Awake()
+    {
+        pc2d = transform.GetComponent<PolygonCollider2D>();
+        //controller = transform.GetComponent<EnemyContrller>();
+        animator = transform.GetComponent<Animator>();
+    }
+}
--- W.S.U.12.5/Assets/Scripts/PlayeManager.cs
+++ W.S.U.12.5/Assets/Scripts/PlayeManager.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayeManager : MonoBehaviour {
+public class PlayeManager : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+    public float speed = 2f;
+    public bool grounded;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Rigidbody2D rb2d;
+    private bool movement = true;
+
+    Vector2 mov;
+
+    void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void FixedUpdate()
+    {
+        Vector3 fixVel = rb2d.velocity;
+        fixVel.x *= 0.75f;
+        if (grounded)
+        {
+            rb2d.velocity = fixVel;
+        }
+
+        mov.x = Input.GetAxis("Horizontal");
+        if (!movement) mov.x = 0;
+        rb2d.AddForce(Vector2.right * speed * mov);
+        float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+        if (mov.x > 0.1f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        if (mov.x < -0.1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
 }
