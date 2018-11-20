c599f851b2564ee7cbfedb316fe964efb3efe65c
jorge luis suarique <jlsuarique@gmail.com>
2018-11-20 16:27:15 -0500

    part10

--- W.S.U.12.5/Assets/Scripts/PlayeManager.cs
+++ W.S.U.12.5/Assets/Scripts/PlayeManager.cs
@@ -38,19 +38,20 @@ public class PlayeManager : MonoBehaviour
         rb2d.AddForce(Vector2.right * speed * mov);
         float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
         rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
-        if (mov.x > 0.1f)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-        if (mov.x < -0.1f)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
+
+        //if (mov.x > 0.1f)
+        //{
+        //    transform.localScale = new Vector3(1f, 1f, 1f);
+        //}
+        //if (mov.x < -0.1f)
+        //{
+        //    transform.localScale = new Vector3(-1f, 1f, 1f);
+        //}
     }
 
     void OnBecameInvisible()
     {
-        transform.position = new Vector2(-1, 0);
-        Debug.Log("e vuelto");
+        transform.position = new Vector2(-1, 1);
+        Debug.Log("he vuelto");
     }
 }
---
+++ W.S.U.12.5/Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    [SerializeField] private GameObject[] gos;
+    [SerializeField] private Transform[] resetT;
+    private int childCount;
+
+
+    private void Awake()
+    {
+        childCount = transform.childCount;
+    }
+
+    private void ResetT()
+    {
+        resetT = new Transform[childCount];
+
+        for (int i = 0; i < childCount; i++)
+        {
+            resetT[i] = transform.GetChild(i);
+        }
+    }
+
+    private void GetGoChilds()
+    {
+        gos = new GameObject[childCount];
+
+        for (int i = 0; i < childCount; i++)
+        {
+            gos[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    private void Start()
+    {
+        GetGoChilds();
+    }
+
+    private void Update()
+    {
+        ResetT();
+    }
+
+    private void OnBecameInvisible()
+    {
+        for (int i = 0; i < childCount; i++)
+        {
+            gos[i].transform.position = resetT[i].position;
+        }
+    }
+}
