07d0d800938b409122070c065849659487259a97
JorgeLuisSuarique <jlsuarique@gmail.com>
2018-11-28 12:44:28 -0500

    part19

---
+++ W.S.U.12.5/Assets/Scripts/CheckGround.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    private PlayeManager play;
+
+    void Start ()
+    {
+       play = GetComponentInParent<PlayeManager>();
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            play.grounded = true;
+        }
+        if (collision.gameObject.tag == "Platform")
+        {
+            play.transform.parent = collision.transform;
+            play.grounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            play.grounded = false;
+        }
+        if (collision.gameObject.tag == "Platform")
+        {
+            play.transform.parent = null;
+            play.grounded = false;
+        }
+    }
+}
