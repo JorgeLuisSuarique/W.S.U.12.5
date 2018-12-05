9d195006c0975ecb2f21d0a1d98325a48aa5ce01
jorge luis suarique <jlsuarique@gmail.com>
2018-12-05 16:51:18 -0500

    part29

---
+++ W.S.U.12.5/Assets/Scripts/Scenes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+
+    public void CargarNivel(string nombreScene)
+    {
+        SceneManager.LoadScene(nombreScene);
+    }
+   
+}
